777b8496aad2d3e0c79c9833b78552a638f60219
Kauê <alexkauezinho@gmail.com>
2024-03-15 20:11:50 -0300

    Lógica Interruptores.

--- Exercicios/Exercicio_4.js
+++ Exercicios/Exercicio_4.js
@@ -5,4 +5,4 @@
 // Assim vou até a sala das lâmpadas.
 // A lâmpada que está acesa é controlada pelo segundo interruptor (o único que deixei ligado).
 // Das duas lâmpadas que estão apagadas, uma estará quente (porque estava ligada há pouco tempo) e a outra estará fria.
-// A lâmpada quente é controlada pelo primeiro interruptor e a lâmpada fria é controlada pelo terceiro interruptor. FAMOSA essa, gostei de lembrar.
+// A lâmpada quente é controlada pelo primeiro interruptor e a lâmpada fria é controlada pelo terceiro interruptor.
